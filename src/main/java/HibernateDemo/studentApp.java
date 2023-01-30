674086d2072f5d3b08f3d433e9b896f674cae196
Chirag Sinha <chirag.sinha@zeotap.com>
2023-01-30 13:22:17 +0530

    save/get data to/from db

--- src/main/java/HibernateDemo/studentApp.java
+++ src/main/java/HibernateDemo/studentApp.java
@@ -11,9 +11,20 @@ public class studentApp {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
         EntityManager em = emf.createEntityManager();
 
-        student s1 = em.find(student.class, 2);
-
+        // To get data from the table
+        student s1 = em.find(student.class, 1);
         System.out.println(s1);
+
+        // To insert data into table
+        student s2 = new student();
+        s2.setId(4);
+        s2.setName("Random");
+        s2.setAge(25);
+
+        em.getTransaction().begin();
+        em.persist(s2);
+        em.getTransaction().commit();
+        System.out.println(s2);
 
     }
 
